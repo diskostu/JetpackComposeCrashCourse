9c25f0a63d87675927c7ec10c47a8539080839ff
Denny Kluge <denny.kluge@gmail.com>
2023-02-08 22:19:40 +0100

    finished the crash course

--- app/src/main/java/de/diskostu/demo/jetpackcomposecrashcourse/MainActivity.kt
+++ app/src/main/java/de/diskostu/demo/jetpackcomposecrashcourse/MainActivity.kt
@@ -3,164 +3,128 @@ package de.diskostu.demo.jetpackcomposecrashcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import de.diskostu.demo.jetpackcomposecrashcourse.ui.theme.JetpackComposeCrashCourseTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeCrashCourseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    LazyColumnDemo()
+                demoMethod2()
+            }
+        }
+    }
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun demoMethod2() {
+
+        var name by remember {
+            mutableStateOf("")
+        }
+
+        var names by remember {
+            mutableStateOf(listOf<String>())
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                OutlinedTextField(
+                    value = name,
+                    singleLine = true,
+                    onValueChange = { text ->
+                        name = text
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = {
+                    if (name.isNotBlank()) {
+                        names = names + name
+                        name = ""
+                    }
+                }) {
+                    Text(
+                        text = "Add"
+                    )
                 }
             }
+
+            NameList(names)
         }
     }
-}
 
-
-@Composable
-fun LazyColumnDemo() {
-    LazyColumn {
-        items(100) {
-            Row {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = null
+    @Composable
+    private fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+        LazyColumn(modifier) {
+            items(names) { currentName ->
+                Text(
+                    text = currentName,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
                 )
-                Text(text = it.toString())
+                Divider()
             }
         }
     }
-}
 
 
-@Preview(showBackground = true)
-@Composable
-fun LazyColumnDemoPreview() {
-    JetpackComposeCrashCourseTheme {
-        LazyColumnDemo()
+    @Preview(showBackground = true)
+    @Composable
+    private fun demoMethod2Preview() {
+        demoMethod2()
     }
-}
 
-@Composable
-fun ImageDemo(name: String) {
-    Column {
-        if (name.length > 5) {
-            Image(
-                painter = painterResource(
-                    id = R.drawable.ic_launcher_foreground
-                ),
-                contentDescription = null,
-                modifier = Modifier.background(Color.Black)
-            )
-        } else {
-            for (i in 1..10) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = null
-                )
-            }
+    @Composable
+    private fun demoMethod1() {
+        var count by remember {
+            mutableStateOf(0)
         }
-    }
-}
 
-
-//@Preview(showBackground = true)
-@Composable
-fun ImageDemoPreview1() {
-    JetpackComposeCrashCourseTheme {
-        ImageDemo("denny")
-    }
-}
-
-
-// @Preview(showBackground = true)
-@Composable
-fun ImageDemoPreview2() {
-    JetpackComposeCrashCourseTheme {
-        ImageDemo("denny<<<")
-    }
-}
-
-
-@Composable
-fun LayoutDemo(name: String) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .background(Color.Gray)
-    ) {
-        Text(
-            text = "Hello $name!",
-            color = Color.Blue,
-            fontSize = 24.sp,
-            modifier = Modifier
-                // modifiers are applied sequentially
-                .background(Color.Red)
-                .padding(16.dp)
-                .background(Color.Green)
-        )
-        Text(
-            text = "Some other text quite long",
-            color = Color.Blue,
-            fontSize = 24.sp,
-        )
-        Box(
-            modifier = Modifier
-                .size(400.dp)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text(
-                text = "Now Box Layout starts",
-                color = Color.Blue,
-                fontSize = 24.sp,
-                modifier = Modifier
-                    // modifiers are applied sequentially
-                    .background(Color.Red)
-                    .padding(16.dp)
-                    .background(Color.Green)
-            )
-            Text(
-                text = "Some other text quite long",
-                color = Color.Blue,
-                fontSize = 24.sp,
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-            )
+            Text(count.toString())
+            Button(onClick = {
+                count++
+            }) {
+                Text(text = "Click me! $count")
+            }
         }
-    }
-}
-
-// @Preview(showBackground = true)
-@Composable
-fun LayoutDemoPreview() {
-    JetpackComposeCrashCourseTheme {
-        LayoutDemo("Denny")
     }
 }
